383676a665838b74f5c9e099e5fd8614b988dc8c
James Austin <jamesaustin@mac.com>
2022-11-30 13:47:57 +0100

    Multithread validation.

--- validate-email-domains.py
+++ validate-email-domains.py
@@ -1,22 +1,42 @@
 #!/usr/bin/env python
+from concurrent.futures import ThreadPoolExecutor, as_completed as futures_as_completed
+
 from dns.resolver import query as domain_query
 from dns.exception import DNSException
 
 FILENAME = 'disposable-email-domains.txt'
-DOMAINS = [ ]
+DOMAINS = set()
+INPUTS = set()
 
 with open(FILENAME) as f:
-    for domain in f:
-        d = domain.strip()
+    for d in f:
+        d = d.strip()
         d = d.strip('.')
+        INPUTS.add(d)
 
+def test_domain(d):
+    try:
+        for _ in domain_query(d, 'MX'):
+            DOMAINS.add(d)
+            return True
+    except DNSException:
+        pass
+    return False
+
+with ThreadPoolExecutor(max_workers=100) as executor:
+    futures = {executor.submit(test_domain, d): d for d in INPUTS}
+    for f in futures_as_completed(futures):
+        d = futures[f]
         try:
-            for _ in domain_query(d, 'MX'):
-                DOMAINS.append(domain)
-                print '+', d
-                break
-        except DNSException:
-            print '-', d
+            success = f.result()
+        except Exception as e:
+            error(f"# Failed: {d} -> {e}")
+        else:
+            if success:
+                print(f"+ {d}")
+            else:
+                print(f"\033[31m- {d}\033[0m")
 
 with open(FILENAME, 'w') as f:
-    f.write(''.join(DOMAINS))
+    f.write('\n'.join(sorted(DOMAINS)))
+
